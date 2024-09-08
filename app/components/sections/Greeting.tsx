338a97b5d11c1781fa75e60c650785b92450fdb1
Ali <alialdarraji095@gmail.com>
2024-09-08 16:38:39 +0300

    greeting section

--- app/components/sections/Greeting.tsx
+++ app/components/sections/Greeting.tsx
@@ -6,50 +6,89 @@ import * as motion from "framer-motion/client"
 
 export default function Greeting() {
     return (
-        <motion.div
-            initial={{
-                scale: 0.25,
-                opacity: 0,
-                y: 144
-            }}
-            whileInView={{
-                scale: 1,
-                opacity: 1,
-                y: 0
-            }}
-            viewport={{ once: true }}
-            transition={{
-                duration: 1,
-                ease: 'easeOut',
-            }}
-            className='relative pb-32'
-        >
-            <img
+        <div className='relative pb-32 max-w-screen-2xl mx-auto'>
+            <motion.img
+                initial={{
+                    opacity: 0,
+                    x: 240
+                }}
+                whileInView={{
+                    opacity: 1,
+                    x: 0
+                }}
+                viewport={{ once: true }}
+                transition={{
+                    duration: 1,
+                    ease: 'easeOut',
+                }}
                 src={WheatImg}
-                className='absolute top-0 xl:hidden'
+                className='absolute bottom-0 z-10'
                 alt='Wheat'
             />
-            <img
+            <motion.img
+                initial={{
+                    opacity: 0,
+                    scaleX: -1,
+                    x: 72
+                }}
+                whileInView={{
+                    opacity: 1,
+                    x: 0
+                }}
+                viewport={{ once: true }}
+                transition={{
+                    duration: 1,
+                    ease: 'easeOut',
+                }}
                 src={WheatImg}
-                className='absolute bottom-0 hidden xl:block'
                 alt='Wheat'
+                className='absolute top-0 left-0 xl:hidden z-10'
             />
-            <img
+            <motion.img
+                initial={{
+                    opacity: 0,
+                    scaleX: -1,
+                    x: 72
+                }}
+                whileInView={{
+                    opacity: 1,
+                    x: 0
+                }}
+                viewport={{ once: true }}
+                transition={{
+                    duration: 1,
+                    ease: 'easeOut',
+                }}
                 src={WheatImg}
                 alt='Wheat'
-                className='reflect absolute bottom-0 left-0'
+                className='absolute bottom-0 left-0 hidden xl:block z-10'
             />
-            <div className='flex flex-col items-center justify-center max-w-7xl mx-auto bg-white border border-gray-200 drop-shadow-lg -my-10 inner-curve'>
+            <motion.div
+                initial={{
+                    scale: 0.25,
+                    opacity: 0,
+                }}
+                whileInView={{
+                    scale: 1,
+                    opacity: 1,
+                }}
+                viewport={{ once: true }}
+                transition={{
+                    duration: 0.5,
+                    ease: 'easeOut',
+                }}
+                className='flex flex-col items-center justify-center max-w-7xl mx-auto bg-white -my-10 inner-curve rounded-lg -z-10'
+            >
                 <div className='flex flex-col items-center justify-center my-20 gap-4 w-full relative bg-white/50 xl:bg-background/0'>
                     <Wheat/>
                     <span>مرحبا بكم في نموذجنا الزراعي</span>
                     <hr className='bg-green pb-[1px] w-8'/>
                     <div className='flex flex-col xl:flex-row items-center justify-between gap-10 px-4 w-full max-w-6xl'>
-                        <div className='flex flex-col items-start justify-center gap-4 max-w-md'>
+                        <div className='flex flex-col items-start justify-center gap-4 max-w-md z-20'>
                             <p className='text-green text-lg'>
                                 <span className='text-xl'>1. </span>الخدمات الزراعية
                             </p>
-                            <span>توفر شركة جنة واسط الزراعية افضل الاستثمارات و الخدمات الزراعية التي تشمل البذور و الاسمدة والحراثة ورعاية الاشجار وازالتها وقص العشب والصيانة المستمرة للمكائن</span>
+                            <span className='xl:bg-white/50 rounded-lg'>توفر شركة جنة واسط الزراعية افضل الاستثمارات و الخدمات الزراعية التي تشمل البذور و الاسمدة والحراثة ورعاية الاشجار وازالتها وقص العشب والصيانة المستمرة للمكائن</span>
                             <Link to='#' className='mt-4'>
                                 <Button variant='primaryGreen'>
                                     اقرا المزيد
@@ -57,11 +96,11 @@ export default function Greeting() {
                             </Link>
                         </div>
                         <div
-                            className='flex flex-col items-start xl:items-end justify-center gap-4 xl:text-left max-w-md'>
+                            className='flex flex-col items-start xl:items-end justify-center gap-4 xl:text-left max-w-md z-20'>
                             <p className='text-green text-lg'>
                                 <span className='text-xl'>2. </span>خدمات الرعاية الحيوانية
                             </p>
-                            <span>توفر شركة جنة واسط مجموعة من الخدمات المخصصة لرعاية الابقار بطريقة تهدف الى تحسين صحتها وزيادة انتاجيتها مثل التغذية الصحية وادترة الانتاج والصحة البيئية والتربية والتدريب</span>
+                            <span className='xl:bg-white/50 rounded-lg'>توفر شركة جنة واسط مجموعة من الخدمات المخصصة لرعاية الابقار بطريقة تهدف الى تحسين صحتها وزيادة انتاجيتها مثل التغذية الصحية وادترة الانتاج والصحة البيئية والتربية والتدريب</span>
                             <Link to='#' className='mt-4'>
                                 <Button variant='primaryGreen'>
                                     اقرا المزيد
@@ -70,7 +109,7 @@ export default function Greeting() {
                         </div>
                     </div>
                 </div>
-            </div>
-        </motion.div>
+            </motion.div>
+        </div>
     );
 }

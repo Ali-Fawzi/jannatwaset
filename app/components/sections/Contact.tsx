14e5993809a34bdc96a8a7ada6131c3fc4fc5726
Ali <alialdarraji095@gmail.com>
2024-09-13 14:49:36 +0300

    contact page

--- app/components/sections/Contact.tsx
+++ app/components/sections/Contact.tsx
@@ -5,68 +5,94 @@ import Email from '../../asstes/icons/ContactEmail.svg'
 import Location from '../../asstes/icons/ContactLocation.svg'
 import * as motion from "framer-motion/client"
 import {animationVariants} from "~/lib/utils";
+import WheatImg from "~/asstes/images/wheat.png";
 
 export default function Contact() {
     return (
-        <div className='flex flex-col items-center justify-center gap-2 px-4'>
-            <motion.div
-                initial={false}
-                variants={animationVariants}
-                whileInView={'visible'}
-                viewport={{once: true}}
-                className='flex flex-col items-center justify-center gap-2 opacity-0'
-            >
-                <ContactIcon />
-                <span>ابق على تواصل معنا</span>
-                <span className='text-3xl'>هل لديك اي سؤال؟</span>
-            </motion.div>
-            <motion.form
-                initial={false}
-                variants={animationVariants}
-                whileInView={'visible'}
-                viewport={{once: true}}
-                className='grid grid-cols-1 xl:grid-cols-2 gap-8 max-w-5xl mx-auto w-full mt-8 justify-items-center opacity-0'
-            >
-                <input
-                    required
-                    name='name'
-                    className='text-black p-4 rounded-lg bg-background focus:outline-none max-w-lg w-full '
-                    placeholder='الاسم'
-                    type='text'
-                />
-                <input
-                    required
-                    name='email'
-                    className='text-black p-4 rounded-lg bg-background focus:outline-none max-w-lg w-full '
-                    placeholder='البريد الالكتروني'
-                    type='email'
-                />
-                <input
-                    required
-                    name='address'
-                    className='text-black p-4 rounded-lg bg-background focus:outline-none max-w-lg w-full '
-                    placeholder='العنوان'
-                    type='text'
-                />
-                <input
-                    required
-                    name='phone'
-                    className='text-black p-4 rounded-lg bg-background focus:outline-none max-w-lg w-full '
-                    placeholder='رقم الهاتف'
-                    type='text'
-                />
-                <textarea
-                    required
-                    name='message'
-                    className='text-black p-4 rounded-lg bg-background focus:outline-none xl:col-span-2 h-64 max-w-lg xl:max-w-5xl w-full'
-                    placeholder='اكتب الرسالة هنا ..'
-                />
-                <div className='mx-auto xl:col-span-2'>
-                    <Button variant='secondaryGreen'>
-                        ارسل الرسالة
-                    </Button>
-                </div>
-            </motion.form>
+        <div className='px-4'>
+            <div className='flex flex-col items-center justify-center gap-2 relative'>
+                <motion.div
+                    initial={{
+                        y: -72
+                    }}
+                    variants={animationVariants}
+                    whileInView={'correctY'}
+                    viewport={{once: true}}
+                    className='absolute top-0 left-0 hidden xl:block opacity-0'
+                >
+                    <img src={WheatImg} alt='Wheat'/>
+                </motion.div>
+                <motion.div
+                    initial={{
+                        y: 72,
+                        scaleX: -1
+                    }}
+                    variants={animationVariants}
+                    whileInView={'correctY'}
+                    viewport={{once: true}}
+                    className='absolute bottom-0 right-0 hidden xl:block opacity-0'
+                >
+                    <img src={WheatImg} alt='Wheat'/>
+                </motion.div>
+                <motion.div
+                    initial={false}
+                    variants={animationVariants}
+                    whileInView={'visible'}
+                    viewport={{once: true}}
+                    className='flex flex-col items-center justify-center gap-2 opacity-0 z-10'
+                >
+                    <ContactIcon/>
+                    <span>ابق على تواصل معنا</span>
+                    <span className='text-3xl'>هل لديك اي سؤال؟</span>
+                </motion.div>
+                <motion.form
+                    initial={false}
+                    variants={animationVariants}
+                    whileInView={'visible'}
+                    viewport={{once: true}}
+                    className='grid grid-cols-1 xl:grid-cols-2 gap-8 max-w-5xl mx-auto w-full mt-8 justify-items-center opacity-0 z-10'
+                >
+                    <input
+                        required
+                        name='name'
+                        className='text-black p-4 rounded-lg bg-background focus:outline-none max-w-lg w-full '
+                        placeholder='الاسم'
+                        type='text'
+                    />
+                    <input
+                        required
+                        name='email'
+                        className='text-black p-4 rounded-lg bg-background focus:outline-none max-w-lg w-full '
+                        placeholder='البريد الالكتروني'
+                        type='email'
+                    />
+                    <input
+                        required
+                        name='address'
+                        className='text-black p-4 rounded-lg bg-background focus:outline-none max-w-lg w-full '
+                        placeholder='العنوان'
+                        type='text'
+                    />
+                    <input
+                        required
+                        name='phone'
+                        className='text-black p-4 rounded-lg bg-background focus:outline-none max-w-lg w-full '
+                        placeholder='رقم الهاتف'
+                        type='text'
+                    />
+                    <textarea
+                        required
+                        name='message'
+                        className='text-black p-4 rounded-lg bg-background focus:outline-none xl:col-span-2 h-64 max-w-lg xl:max-w-5xl w-full'
+                        placeholder='اكتب الرسالة هنا ..'
+                    />
+                    <div className='mx-auto xl:col-span-2'>
+                        <Button variant='secondaryGreen'>
+                            ارسل الرسالة
+                        </Button>
+                    </div>
+                </motion.form>
+            </div>
             <motion.div
                 initial={false}
                 variants={animationVariants}
@@ -74,25 +100,28 @@ export default function Contact() {
                 viewport={{once: true}}
                 className='mt-16 w-full max-w-6xl mx-auto flex flex-col xl:flex-row gap-8 items-center justify-center opacity-0'
             >
-                <div className='border border-green rounded-lg py-12 flex flex-col items-center justify-center gap-2 w-full max-w-xs'>
+                <div
+                    className='border border-green rounded-lg py-12 flex flex-col items-center justify-center gap-2 w-full max-w-xs'>
                     <div className='p-4 rounded-full bg-background'>
-                        <Telephone />
+                        <Telephone/>
                     </div>
                     <span className='font-semibold'>اتصل بنا</span>
                     <span>07800800758</span>
                     <span>07800800758</span>
                 </div>
-                <div className='border border-green rounded-lg py-12 flex flex-col items-center justify-center gap-2 w-full max-w-xs'>
+                <div
+                    className='border border-green rounded-lg py-12 flex flex-col items-center justify-center gap-2 w-full max-w-xs'>
                     <div className='p-4 rounded-full bg-background'>
-                        <Email />
+                        <Email/>
                     </div>
                     <span className='font-semibold'>ارسل ايميل</span>
                     <span>needhelp@organik.com</span>
                     <span>info@company.com</span>
                 </div>
-                <div className='border border-green rounded-lg py-12 flex flex-col items-center justify-center gap-2 w-full max-w-xs'>
+                <div
+                    className='border border-green rounded-lg py-12 flex flex-col items-center justify-center gap-2 w-full max-w-xs'>
                     <div className='p-4 rounded-full bg-background'>
-                        <Location />
+                        <Location/>
                     </div>
                     <span className='font-semibold'>زورنا للموقع</span>
                     <span>العراق , واسط</span>
